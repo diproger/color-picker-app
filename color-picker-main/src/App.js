6cc42afc57a67fa27fe63e6b5996986ebbac3ee1
diproger <diprogerus@gmail.com>
2020-01-13 18:30:20 +0500

    added Router

--- color-picker-main/src/App.js
+++ color-picker-main/src/App.js
@@ -3,13 +3,20 @@ import {Route, Switch} from 'react-router-dom'
 import Palette from './Palette';
 import { generatePalette } from './colorHelpers'
 import seedColors from './seedColors'
+
 function App() {
 
-  // console.log(generatePalette(seedColors[4]))
+   function findPalette(id) {
+    return seedColors.find(function(palette){
+      return palette.id === id
+    })
+  }
   return (
     <Switch>
           <Route exact path="/" render={() => <h1>Palette list goes here</h1>} />
-          <Route exact path="/palette/:id" render={ () => <h1>one palette</h1> } />
+          <Route exact path="/palette/:id" render={ routeProps => <Palette
+           palette={generatePalette(findPalette(routeProps.match.params.id))}
+           /> } />
     </Switch>
 
     // <div className="App">
