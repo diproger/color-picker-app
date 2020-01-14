0a5bf94dc2ec523a1eb259e925e4072e25df0189
diproger <diprogerus@gmail.com>
2020-01-14 12:02:31 +0500

    add  Link to Palette

--- color-picker-main/src/App.js
+++ color-picker-main/src/App.js
@@ -14,7 +14,7 @@ function App() {
   }
   return (
     <Switch>
-          <Route exact path="/" render={() => <PaletteList palettes={seedColors} />} />
+          <Route exact path="/" render={(routeProps) => <PaletteList palettes={seedColors} {...routeProps} />} />
           <Route exact path="/palette/:id" render={ routeProps => <Palette
            palette={generatePalette(findPalette(routeProps.match.params.id))}
            /> } />
--- color-picker-main/src/MiniPalette.js
+++ color-picker-main/src/MiniPalette.js
@@ -52,7 +52,7 @@ function MiniPalette(props){
         <div className={classes.miniColor} style={{background:color.color}} key={color.name}></div>
     ))
     return (
-        <div className={classes.root}>
+        <div className={classes.root} onClick={props.handleClick}>
             <div className={classes.colors}>
                 {miniColorBoxes}
             </div>
